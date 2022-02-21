4454e3fd76ab2d2bcf0e4ca0fee8ace3fbb2aefc
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-21 15:08:47 -0600

    Hacked in HashRouter...
    Actually worked better than I thought....

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import { Routes, Route } from 'react-router-dom';
 
 const Home = () => <h2>Home</h2>;
 const SetupGame = () => <h2>Setup Game</h2>;
@@ -9,12 +10,11 @@ const PlayGame = () => <h2>Play Game</h2>;
 function App() {
   return (
     <div className="App">
-      <Button variant="outlined">
-        Play
-      </Button>
-      <Home />
-      <SetupGame />
-      <PlayGame />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="setup" element={<SetupGame />} />
+        <Route path="play" element={<PlayGame />} />
+      </Routes>
     </div>
   );
 }
