d5f8ecb338ba06ede24b928523cd081fe183d9db
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-28 15:15:01 -0600

    Passing game results to home screen, and showing first fun fact or stat ! ! !

--- src/App.js
+++ src/App.js
@@ -5,11 +5,36 @@ import { Home } from './Home';
 import { SetupGame } from './SetupGame';
 import { PlayGame } from './PlayGame';
 
-function App() {
+
+
+const game1 = {
+  start: "2022-02-14T15:14:30"
+  , end: "2022-02-14T15:20:00"
+  , winner: "Me"
+  , players: [{ name: "Me", order: 1}, { name: "Taylor", order: 2}, {name: "Jack", order: 3}]
+};
+
+const game2 = {
+  start: "2022-02-14T21:00:30"
+  , end: "2022-02-14T21:30:30"
+  , winner: "Stephanie"
+  , players: [{ name: "Me", order: 1}, { name: "Stephanie", order: 2}, {name: "Jack", order: 3}]
+};
+
+const gameResults = [
+  game1
+  , game2
+];
+
+const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <Home 
+            gameResults={gameResults}
+          />
+        } />
         <Route path="setup" element={<SetupGame />} />
         <Route path="play" element={<PlayGame />} />
       </Routes>
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 
-export const Home = () => {
+export const Home = ({gameResults}) => {
 
     const nav = useNavigate();
 
@@ -10,6 +10,9 @@ export const Home = () => {
             <h2>
                 Home
             </h2>
+            <h3>
+                Total games played: {gameResults.length}
+            </h3>
             <Button
                 variant='outlined'
                 onClick={() => nav("/setup")}
